853aff0ae8840176d602ec3f21c935d31980d5f0
youke1987 <yuji6@126.com>
2016-12-06 02:42:15 +0800

    commit

--- src/main/java/cn/apopo/springboot/helloworld/HelloWorldApplication.java
+++ src/main/java/cn/apopo/springboot/helloworld/HelloWorldApplication.java
@@ -3,7 +3,7 @@ package cn.apopo.springboot.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  // 开启组件扫描和自动配置
+@SpringBootApplication       // 开启组件扫描和自动配置
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
